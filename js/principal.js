6aabecfff493947424b085966999350282571156
JaramilloCris <groso_xx@hotmail.com>
2020-09-16 01:36:25 -0300

    Agregando formulario

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,29 @@
 function hideInput(selectId,inputId){
 
-            var d = document.getElementById(selectId);
-            var textSeleted=d.options[d.selectedIndex].text;
-            document.getElementById(inputId).hidden = textSeleted !== "Otro";
-        }
+    var d = document.getElementById(selectId);
+    var textSeleted=d.options[d.selectedIndex].text;
+    document.getElementById(inputId).hidden = textSeleted !== "Otro";
+}
+
+var numFile = 0;
+
+function addInputFile(num){
+
+    if(numFile<num) {
+        var container = document.getElementById("container-file")
+        var inputFile = document.createElement("input");
+        inputFile.type = "file";
+        container.appendChild(inputFile);
+        container.appendChild(document.createElement("br"));
+        numFile = numFile + 1;
+    }
+}
+
+function clickSub(){
+
+    var button = document.getElementById("subm")
+    button.style.background = '#000000'
+
+}
+
+
